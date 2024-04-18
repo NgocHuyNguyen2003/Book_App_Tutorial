07235acddce7fd582113bfcb27271a597a1b56c1
Huy Nguyen Ngoc <huynn.nguyenngochuy@gmail.com>
2024-04-18 22:12:49 +0700

    Display data

--- app/src/main/java/com/example/book_app/MainActivity.java
+++ app/src/main/java/com/example/book_app/MainActivity.java
@@ -1,17 +1,25 @@
 package com.example.book_app;
 
 import android.content.Intent;
+import android.database.Cursor;
 import android.os.Bundle;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
     RecyclerView recyclerView;
     FloatingActionButton add_button;
+    MyDatabaseHelper myDatabaseHelper;
+    ArrayList<String> book_id,book_title, book_author, book_pages;
 
+    MyAdapter myAdapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -23,6 +31,32 @@ public class MainActivity extends AppCompatActivity {
             Intent intent = new Intent(MainActivity.this, AddActivity.class);
             startActivity(intent);
         });
+
+        myDatabaseHelper = new MyDatabaseHelper(MainActivity.this);
+        book_id = new ArrayList<>();//0
+        book_title = new ArrayList<>();//1
+        book_author = new ArrayList<>();
+        book_pages = new ArrayList<>();
+        store_all_data_inArrays();
+        myAdapter=new MyAdapter(MainActivity.this,book_id, book_title, book_author, book_pages);
+        recyclerView.setAdapter(myAdapter);//gán vào recyclerView
+        recyclerView.setLayoutManager(new LinearLayoutManager(MainActivity.this));//display
+
+    }
+    void store_all_data_inArrays(){
+        Cursor cursor = myDatabaseHelper.read_display_AllData();
+        if(cursor.getCount() == 0)
+        {
+            Toast.makeText(this, "no data found", Toast.LENGTH_SHORT).show();
+        }
+        else
+        {
+            while (cursor.moveToNext()){
+                book_id.add(cursor.getString(0));
+                book_title.add(cursor.getString(1));
+                book_author.add(cursor.getString(2));
+                book_pages.add(cursor.getString(3));
+            }
+        }
     }
 }
-//test commit
--- app/src/main/java/com/example/book_app/MyDatabaseHelper.java
+++ app/src/main/java/com/example/book_app/MyDatabaseHelper.java
@@ -2,6 +2,7 @@ package com.example.book_app;
 
 import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 import android.widget.Toast;
@@ -9,7 +10,7 @@ import android.widget.Toast;
 import androidx.annotation.Nullable;
 
  class MyDatabaseHelper extends SQLiteOpenHelper {
-    private  Context context;
+    private Context context;
     private static final String DATABSE_NAME ="book_library.db";
     private static final int DATABASE_VERSION =1;
     private static final String TABLE_NAME="my_library";
@@ -51,5 +52,16 @@ import androidx.annotation.Nullable;
             Toast.makeText(context, "Added Successfully!", Toast.LENGTH_SHORT).show();
         }
 
+    }
+    Cursor read_display_AllData(){
+        String query = "SELECT * FROM " + TABLE_NAME;
+        SQLiteDatabase sqLiteDatabase = this.getReadableDatabase();
+        Cursor cursor =null;
+        if (sqLiteDatabase != null) // save rs
+        {
+
+            cursor=sqLiteDatabase.rawQuery(query,null);//query
+        }
+        return cursor;
     }
 }
