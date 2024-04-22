1bb0c12d5b412179cbae2d86eed5970fbf372288
Huy Nguyen Ngoc <huynn.nguyenngochuy@gmail.com>
2024-04-22 23:40:16 +0700

    delete

--- app/src/main/java/com/example/book_app/MainActivity.java
+++ app/src/main/java/com/example/book_app/MainActivity.java
@@ -1,11 +1,19 @@
 package com.example.book_app;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.database.Cursor;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.Nullable;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
@@ -19,12 +27,18 @@ public class MainActivity extends AppCompatActivity {
     FloatingActionButton add_button;
     MyDatabaseHelper myDatabaseHelper;
     ArrayList<String> book_id,book_title, book_author, book_pages;
-
+    Button delete_button1;
     MyAdapter myAdapter;
+    ImageView empty_imageview;
+    TextView no_data;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        delete_button1 = findViewById(R.id.delete_all1);
+        empty_imageview = findViewById(R.id.empty_imageview);
+        no_data=findViewById(R.id.no_data);
+
 
         recyclerView = findViewById(R.id.recyclerView);
         add_button = findViewById((R.id.add_button));
@@ -32,6 +46,14 @@ public class MainActivity extends AppCompatActivity {
             Intent intent = new Intent(MainActivity.this, AddActivity.class);
             startActivity(intent);
         });
+
+        delete_button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                confirmDialog1();
+            }
+        });
+
 
         myDatabaseHelper = new MyDatabaseHelper(MainActivity.this);
         book_id = new ArrayList<>();//0
@@ -54,12 +76,19 @@ public class MainActivity extends AppCompatActivity {
             recreate();
         }
     }//generate
-
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {//delete menu
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.my_menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
     void store_all_data_inArrays(){
         Cursor cursor = myDatabaseHelper.read_display_AllData();
         if(cursor.getCount() == 0)
         {
-            Toast.makeText(this, "no data found", Toast.LENGTH_SHORT).show();
+            empty_imageview.setVisibility(View.VISIBLE);
+            no_data.setVisibility(View.VISIBLE);
+            //Toast.makeText(this, "no data found", Toast.LENGTH_SHORT).show();
         }
         else
         {
@@ -69,6 +98,31 @@ public class MainActivity extends AppCompatActivity {
                 book_author.add(cursor.getString(2));
                 book_pages.add(cursor.getString(3));
             }
+            empty_imageview.setVisibility(View.GONE);
+            no_data.setVisibility(View.GONE);
         }
+    }
+    void confirmDialog1() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("Delete All?");
+        builder.setMessage("Are you sure you want to delete all Data?");
+        builder.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                MyDatabaseHelper myDB = new MyDatabaseHelper(MainActivity.this);
+                myDB.deleteAllData();
+                //Refresh Activity
+                Intent intent = new Intent(MainActivity.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+        builder.setNegativeButton("No", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+
+            }
+        });
+        builder.create().show();
     }
 }
