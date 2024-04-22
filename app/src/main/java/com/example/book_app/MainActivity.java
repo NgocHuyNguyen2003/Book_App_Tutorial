dbf7490572599aa41a5246a698e777aa711d4532
Huy Nguyen Ngoc <huynn.nguyenngochuy@gmail.com>
2024-04-22 23:09:06 +0700

    Delete data

--- app/src/main/java/com/example/book_app/MainActivity.java
+++ app/src/main/java/com/example/book_app/MainActivity.java
@@ -1,11 +1,19 @@
 package com.example.book_app;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.database.Cursor;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
@@ -18,13 +26,19 @@ public class MainActivity extends AppCompatActivity {
     RecyclerView recyclerView;
     FloatingActionButton add_button;
     MyDatabaseHelper myDatabaseHelper;
-    ArrayList<String> book_id,book_title, book_author, book_pages;
+    ArrayList<String> book_id, book_title, book_author, book_pages;
 
     MyAdapter myAdapter;
+    Button delete_button1;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        delete_button1 = findViewById(R.id.delete_all1);
+
+
 
         recyclerView = findViewById(R.id.recyclerView);
         add_button = findViewById((R.id.add_button));
@@ -33,13 +47,23 @@ public class MainActivity extends AppCompatActivity {
             startActivity(intent);
         });
 
+        delete_button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                confirmDialog();
+            }
+        });
+
+
+
+
         myDatabaseHelper = new MyDatabaseHelper(MainActivity.this);
         book_id = new ArrayList<>();//0
         book_title = new ArrayList<>();//1
         book_author = new ArrayList<>();
         book_pages = new ArrayList<>();
         store_all_data_inArrays();
-        myAdapter=new MyAdapter(MainActivity.this,this, book_id, book_title, book_author,
+        myAdapter = new MyAdapter(MainActivity.this, this, book_id, book_title, book_author,
                 book_pages);
         recyclerView.setAdapter(myAdapter);//gán vào recyclerView
         recyclerView.setLayoutManager(new LinearLayoutManager(MainActivity.this));//display
@@ -50,25 +74,61 @@ public class MainActivity extends AppCompatActivity {
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
 
-        if (requestCode==1){
+        if (requestCode == 1) {
             recreate();
         }
     }//generate
 
-    void store_all_data_inArrays(){
+    void store_all_data_inArrays() {
         Cursor cursor = myDatabaseHelper.read_display_AllData();
-        if(cursor.getCount() == 0)
-        {
+        if (cursor.getCount() == 0) {
             Toast.makeText(this, "no data found", Toast.LENGTH_SHORT).show();
-        }
-        else
-        {
-            while (cursor.moveToNext()){
+        } else {
+            while (cursor.moveToNext()) {
                 book_id.add(cursor.getString(0));
                 book_title.add(cursor.getString(1));
                 book_author.add(cursor.getString(2));
                 book_pages.add(cursor.getString(3));
             }
         }
+    }
+
+ /*   @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.my_menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }*/
+
+ /*   @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if(item.getItemId() == R.id.delete_all){
+            confirmDialog();
+        }
+        return super.onOptionsItemSelected(item);
+    }
+*/
+    void confirmDialog(){
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("Delete All ?");
+        builder.setMessage("Are you want to delete all data ?");
+        builder.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                MyDatabaseHelper myDB = new MyDatabaseHelper(MainActivity.this);
+                myDB.deleteAllData();
+                //Refresh Activity
+                Intent intent = new Intent(MainActivity.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+        builder.setNegativeButton("No", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+
+            }
+        });
+        builder.create().show();
     }
 }
